a19276075c30ad8e51f5e30205df9da2f35a1b10
PhilipHsu609 <mpyhacct118113@gmail.com>
2024-06-19 22:45:12 +0800

    Use epoll instead of poll

--- include/utils.hpp
+++ include/utils.hpp
@@ -6,6 +6,7 @@
 
 constexpr std::uint16_t PORT = 1234;
 constexpr std::size_t COMMAND_SIZE = 4;
+constexpr int MAX_EVENTS = 10;
 
 void set_nonblocking(int fd);
 std::int32_t read_all(int fd, std::vector<std::byte> &buf, std::size_t n);
--- src/server.cpp
+++ src/server.cpp
@@ -3,6 +3,7 @@
 #include <dbg.h>        // dbg
 #include <fmt/ranges.h> // fmt::print
 
+#include <array>       // std::array
 #include <cerrno>      // errno
 #include <cstddef>     // std::byte, std::size_t
 #include <cstdint>     // std::uint8_t
@@ -14,6 +15,7 @@
 
 #include <netinet/in.h> // sockaddr_in
 #include <poll.h>       // poll, pollfd, nfds_t
+#include <sys/epoll.h>
 #include <sys/socket.h> // accept4, bind, listen, socket, sockaddr
 #include <sys/types.h>  // ssize_t
 #include <unistd.h>     // close, read, write, socklen_t
@@ -45,13 +47,13 @@ void add_connection(std::vector<std::unique_ptr<Connection>> &connections, int f
     connections[fd] = std::make_unique<Connection>(fd);
 }
 
-std::int32_t accept_new_connection(std::vector<std::unique_ptr<Connection>> &connections,
-                                   int fd) {
+int accept_new_connection(std::vector<std::unique_ptr<Connection>> &connections,
+                          int listen_fd) {
     sockaddr_in addr{};
     socklen_t addrlen = sizeof(addr);
 
     const int client_fd =
-        accept4(fd, reinterpret_cast<sockaddr *>(&addr), &addrlen, SOCK_CLOEXEC);
+        accept4(listen_fd, reinterpret_cast<sockaddr *>(&addr), &addrlen, SOCK_CLOEXEC);
     if (client_fd == -1) {
         fmt::print(stderr, "accept failed: {}\n", std::strerror(errno));
         return -1;
@@ -60,7 +62,7 @@ std::int32_t accept_new_connection(std::vector<std::unique_ptr<Connection>> &con
     set_nonblocking(client_fd);
     add_connection(connections, client_fd);
 
-    return 0;
+    return client_fd;
 }
 
 bool try_flush_buffer(std::unique_ptr<Connection> &conn) {
@@ -70,7 +72,6 @@ bool try_flush_buffer(std::unique_ptr<Connection> &conn) {
         // Write as much data as possible, up to the buffer size
         const std::size_t remain = conn->wbuf_size - conn->wbuf_sent;
         n = write(conn->fd, conn->wbuf.data() + conn->wbuf_sent, remain);
-        dbg("write", n, "bytes");
     } while (n < 0 && errno == EINTR);
 
     if (n < 0 && errno == EAGAIN) {
@@ -151,9 +152,9 @@ bool try_fill_buffer(std::unique_ptr<Connection> &conn) {
         // Read as much data as possible, up to the buffer size
         const std::size_t remain = conn->rbuf.size() - conn->rbuf_size;
         n = read(conn->fd, conn->rbuf.data() + conn->rbuf_size, remain);
-    } while (n < 0 && errno == EINTR); // Interrupt occurred before read
+    } while (n == -1 && errno == EINTR); // Interrupt occurred before read
 
-    if (n < 0 && errno == EAGAIN) {
+    if (n == -1 && errno == EAGAIN) {
         // Resource temporarily unavailable, try again later
         return false;
     }
@@ -188,14 +189,14 @@ void connection_io(std::unique_ptr<Connection> &conn) {
 }
 
 int main() {
-    const int fd = socket(AF_INET, SOCK_STREAM, 0);
-    if (fd == -1) {
+    const int listen_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (listen_fd == -1) {
         fmt::print(stderr, "socket failed: {}\n", std::strerror(errno));
         return 1;
     }
 
     int val = 1;
-    if (setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &val, sizeof(val)) != 0) {
+    if (setsockopt(listen_fd, SOL_SOCKET, SO_REUSEADDR, &val, sizeof(val)) == -1) {
         fmt::print(stderr, "setsockopt failed: {}\n", std::strerror(errno));
         return 1;
     }
@@ -205,60 +206,65 @@ int main() {
     addr.sin_port = ntohs(PORT);
     addr.sin_addr.s_addr = ntohl(0);
 
-    if (bind(fd, reinterpret_cast<const sockaddr *>(&addr), sizeof(addr)) != 0) {
+    if (bind(listen_fd, reinterpret_cast<const sockaddr *>(&addr), sizeof(addr)) == -1) {
         fmt::print(stderr, "bind failed: {}\n", std::strerror(errno));
         return 1;
     }
 
-    if (listen(fd, SOMAXCONN) != 0) {
+    if (listen(listen_fd, SOMAXCONN) == -1) {
         fmt::print(stderr, "listen failed: {}\n", std::strerror(errno));
         return 1;
     }
 
     fmt::print("Listening on port {}\n", PORT);
 
+    set_nonblocking(listen_fd);
     std::vector<std::unique_ptr<Connection>> connections; // index is fd
-    set_nonblocking(fd);
+    std::array<epoll_event, MAX_EVENTS> events{};
 
-    std::vector<pollfd> pollfds;
+    const int epfd = epoll_create1(EPOLL_CLOEXEC);
+    if (epfd == -1) {
+        fmt::print(stderr, "epoll_create1 failed: {}\n", std::strerror(errno));
+        return 1;
+    }
+
+    epoll_event ev{};
+    ev.events = EPOLLIN;
+    ev.data.fd = listen_fd;
+
+    // Add the listening socket to the epoll set
+    if (epoll_ctl(epfd, EPOLL_CTL_ADD, listen_fd, &ev) == -1) {
+        dbg(fmt::format("epoll_ctl failed: {}", std::strerror(errno)));
+        return 1;
+    }
+
     while (true) {
-        pollfds.clear();
-
-        const pollfd pfd{fd, POLLIN, 0};
-        pollfds.push_back(pfd);
-
-        for (auto &conn : connections) {
-            if (conn == nullptr) {
-                continue;
-            }
-
-            pollfd temp_pfd{};
-            temp_pfd.fd = conn->fd;
-            temp_pfd.events = (conn->state == State::REQUEST) ? POLLIN : POLLOUT;
-
-            pollfds.push_back(temp_pfd);
-        }
-
-        const int nready =
-            poll(pollfds.data(), static_cast<nfds_t>(pollfds.size()), 1000);
+        const int nready = epoll_wait(epfd, events.data(), MAX_EVENTS, -1);
         if (nready < 0) {
-            fmt::print(stderr, "poll failed: {}\n", std::strerror(errno));
+            fmt::print(stderr, "epoll_wait failed: {}\n", std::strerror(errno));
             return 1;
         }
 
-        for (std::size_t i = 1; i < pollfds.size(); ++i) {
-            if (pollfds[i].revents != 0) {
-                auto &conn = connections[pollfds[i].fd];
+        dbg(nready);
+        for (int i = 0; i < nready; ++i) {
+            if (events[i].data.fd == listen_fd) {
+                const int client_fd = accept_new_connection(connections, listen_fd);
+
+                // Add the new connection to the epoll set
+                ev.events = EPOLLIN | EPOLLET;
+                ev.data.fd = client_fd;
+                if (epoll_ctl(epfd, EPOLL_CTL_ADD, client_fd, &ev) == -1) {
+                    dbg(fmt::format("epoll_ctl failed: {}", std::strerror(errno)));
+                    return 1;
+                }
+            } else {
+                auto &conn = connections[events[i].data.fd];
                 connection_io(conn);
                 if (conn->state == State::END) {
                     close(conn->fd);
-                    connections[pollfds[i].fd].reset();
+                    conn.reset();
                 }
             }
-        }
-
-        if (pollfds[0].revents != 0) {
-            accept_new_connection(connections, fd);
         }
     }
 }
